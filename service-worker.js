b03e8e19076e6a6291a3d51aef87ce28204a8424
abhishek-18081990 <abhishek.counsellor@gmail.com>
2025-04-23 12:23:39 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+// public/service-worker.js
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('astro-app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
