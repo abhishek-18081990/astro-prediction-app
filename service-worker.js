7846dc13b5bd183669f7b1458d38192b42de38d7
abhishek-18081990 <abhishek.counsellor@gmail.com>
2025-04-23 13:30:47 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,24 @@
-const CACHE_NAME = "astro-cache-v1";
-const urlsToCache = ["/", "/index.html", "/manifest.json"];
+const cacheName = 'jyotish-cache-v1';
+const staticAssets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async event => {
+  const req = event.request;
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
 });
